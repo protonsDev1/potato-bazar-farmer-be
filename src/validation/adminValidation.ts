b5e003dbfffdd2fde0bd8af53fc79876dd56a3b4
Prassana181 <prassana.pandya@protonshub.in>
2025-06-12 16:29:51 +0530

    fix: some changes in crud common function

--- src/validation/adminValidation.ts
+++ src/validation/adminValidation.ts
@@ -4,47 +4,56 @@ export const irrigationSourceSchema = Joi.object({
   name: Joi.string().required(),
   icon: Joi.string().optional(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const soilTypeSchema = Joi.object({
   name: Joi.string().required(),
   icon: Joi.string().optional(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const potatoVarietySchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const potatoSubVarietySchema = Joi.object({
   name: Joi.string().required(),
   varietyId: Joi.number().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const sowingMethodSchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const farmEquipmentUsedSchema = Joi.object({
   name: Joi.string().required(),
   icon: Joi.string().optional(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const technologyUsedSchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const priceDiscoverySchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
 
 export const biggestChallengeInSellingSchema = Joi.object({
   name: Joi.string().required(),
   position: Joi.number().optional(),
+  isActive: Joi.boolean().optional()
 });
