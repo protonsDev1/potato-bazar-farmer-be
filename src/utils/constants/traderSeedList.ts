21a5c6540b9a1d687da6ac257dead656f1a1a15b
Akash Mishra <akash.mishra@protonshub.in>
2025-07-08 19:25:27 +0530

    Add seed data for trader modules

---
+++ src/utils/constants/traderSeedList.ts
@@ -0,0 +1,33 @@
+export const marketCoverageList = [
+  { name: "Local Mandi", position: 1 },
+  { name: "District", position: 2 },
+  { name: "Major State", position: 3 },
+];
+
+export const traderTypeList = [
+  { type: "Mandi Agent", position: 1 },
+  { type: "Trader/Wholesaler", position: 2 },
+  { type: "Exporter", position: 3 },
+];
+
+export const cropTradedList = [
+  { cropName: "Potato", position: 1 },
+  { cropName: "Onion", position: 2 },
+  { cropName: "Tomato", position: 3 },
+  { cropName: "Garlic", position: 4 },
+  { cropName: "Ginger", position: 5 },
+  { cropName: "Other", position: 6 },
+];
+
+export const traderInterestList = [
+  { interest: "Buy Produce", position: 1 },
+  { interest: "Post RFQ", position: 2 },
+  { interest: "Join Auction", position: 3 },
+];
+
+export const traderVarietyList = [
+  { variety: "Table", position: 1 },
+  { variety: "Chips", position: 2 },
+  { variety: "French Fries", position: 3 },
+  { variety: "Seed", position: 4 },
+];
