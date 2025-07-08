4f2461b5747304c4ae52b90e454d4cd4f8f53932
Akash Mishra <akash.mishra@protonshub.in>
2025-07-08 11:33:37 +0530

    Implement cold storage update API with role based checks

--- src/routes/coldStorageRoutes.ts
+++ src/routes/coldStorageRoutes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { createValidator } from 'express-joi-validation';
 import { adminMiddleware, authMiddleware } from '../utils/userAuth';
-import { coldStorageSchema } from '../validation/coldStorageValidation';
-import { createColdStorage, getColdStorageProfile, getColdStorageList, selfOnboardColdStorage,  } from '../controller/coldStorage';
+import { coldStorageSchema, updateColdStorageSchema } from '../validation/coldStorageValidation';
+import { createColdStorage, getColdStorageProfile, getColdStorageList, selfOnboardColdStorage, updateColdStorage,  } from '../controller/coldStorage';
 
 const router = express.Router();
 const validator = createValidator({});
@@ -18,6 +18,12 @@ router.post('/self_onboard',validator.body(coldStorageSchema),selfOnboardColdSto
 
 router.get("/profile/:id", authMiddleware, getColdStorageProfile);
 
+router.put(
+  '/update/:coldStorageId',
+  authMiddleware,
+  validator.body(updateColdStorageSchema),
+  updateColdStorage
+);
 router.get("/", adminMiddleware, getColdStorageList);
 
 export default router;
