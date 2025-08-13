f2b87de49d8eb897fc044984e92c329f750587fe
Prassana181 <prassana.pandya@protonshub.in>
2025-08-13 15:15:58 +0530

    feat: Mandi agent admin crud implemented

--- src/utils/userAuth.ts
+++ src/utils/userAuth.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User, { USER_ROLES } from "../database/models/user";
+import SubAdminPermission from "../database/models/subAdminPermission";
 
 dotenv.config();
 
@@ -121,6 +122,54 @@ export const adminMiddleware = async (req, res, next) => {
   }
 };
 
+export const checkPermissionMiddleware = (permission: string) => {
+  return async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Access Denied: No Token Provided" });
+    }
+
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET) as { id: string };
+
+      const user = await User.findByPk(decoded.id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      // Attach user to request object
+      req.user = user;
+
+      switch (user.role) {
+        case USER_ROLES.SUPER_ADMIN:
+          return next(); // always allowed
+
+        case USER_ROLES.SUB_ADMIN: {
+          const exists = await SubAdminPermission.findOne({
+            where: { userId: user.id, permission },
+          });
+          if (!exists) {
+            return res.status(403).json({
+              message: `${USER_ROLES.SUB_ADMIN} does not have '${permission}' permission.`,
+            });
+          }
+          return next();
+        }
+
+        default:
+          return res.status(403).json({
+            message: `Only ${USER_ROLES.SUPER_ADMIN} and ${USER_ROLES.SUB_ADMIN} with valid permission are authorized.`,
+          });
+      }
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid or Expired Token" });
+    }
+  };
+};
+
 export const checkOtpVerified = async (req, res, next) => {
   const { mobile } = req.body;
 
@@ -131,3 +180,4 @@ export const checkOtpVerified = async (req, res, next) => {
 
   next();
 };
+
