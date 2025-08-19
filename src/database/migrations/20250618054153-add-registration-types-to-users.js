d775a0d5dd1b2c3e9f6b2088523aace9e96056a7
protonsDev1 <shubham.joshi@protonshub.com>
2025-08-19 15:49:38 +0530

    added api for kyc

--- src/database/migrations/20250618054153-add-registration-types-to-users.js
+++ src/database/migrations/20250618054153-add-registration-types-to-users.js
@@ -3,9 +3,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('users', 'registration_types', {
-      type: Sequelize.STRING, // or Sequelize.ARRAY(Sequelize.STRING) if multiple values
-      allowNull: true,        // change to false if required
-      defaultValue: null      // or set a default value
+      type: Sequelize.STRING, 
+      allowNull: true,        
+      defaultValue: null     
     });
   },
 
