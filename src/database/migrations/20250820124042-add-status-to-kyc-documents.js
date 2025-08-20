a3cd322bbc5a5beaee02dd23f4903bd2f904d6b0
protonsDev1 <shubham.joshi@protonshub.com>
2025-08-20 18:26:14 +0530

    added status key

--- src/database/migrations/20250820124042-add-status-to-kyc-documents.js
+++ src/database/migrations/20250820124042-add-status-to-kyc-documents.js
@@ -4,8 +4,8 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('kyc_documents', 'status', {
       type: Sequelize.STRING,
-      allowNull: true, // or false if you want it required
-      defaultValue: 'pending' // you can set default (optional)
+      allowNull: true, 
+      defaultValue: 'pending' 
     });
   },
 
