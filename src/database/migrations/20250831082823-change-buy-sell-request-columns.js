10c5de5f3146d7785e77c0173bf4d5eed327b51e
Akash Mishra <akash.mishra@protonshub.in>
2025-08-31 14:29:08 +0530

    fix: Modify buy and sell request validations

---
+++ src/database/migrations/20250831082823-change-buy-sell-request-columns.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("buyRequests", "size", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.changeColumn("sellRequests", "size", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.changeColumn("buyRequests", "size", {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+    await queryInterface.changeColumn("sellRequests", "size", {
+      type: Sequelize.FLOAT,
+      allowNull: true,
+    });
+  },
+};
