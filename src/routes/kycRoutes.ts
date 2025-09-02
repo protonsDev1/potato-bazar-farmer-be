5d5b116d5c2c3053949c82b8781b3986ef899227
Akash Mishra <akash.mishra@protonshub.in>
2025-09-02 17:07:45 +0530

    Allow kyc and agent performance permissions to sub admin

--- src/routes/kycRoutes.ts
+++ src/routes/kycRoutes.ts
@@ -1,15 +1,33 @@
-import express from 'express';
+import express from "express";
 import { createValidator } from "express-joi-validation";
-import { createKycSchema, updateKycStatusSchema } from '../validation/kycValidation';
-import { createKyc, approveOrRejectKyc, listKyc, getKycDetail } from '../controller/kycController';
-import { authMiddleware } from '../utils/userAuth';
+import {
+  createKycSchema,
+  updateKycStatusSchema,
+} from "../validation/kycValidation";
+import {
+  createKyc,
+  approveOrRejectKyc,
+  listKyc,
+  getKycDetail,
+} from "../controller/kycController";
+import { authMiddleware, checkPermissionMiddleware } from "../utils/userAuth";
+import { PERMISSIONS } from "../utils/constants/permissions";
 
 const router = express.Router();
-const validator = createValidator({});  
+const validator = createValidator({});
 
-router.post('/',authMiddleware, validator.body(createKycSchema), createKyc);
-router.patch('/:id', validator.body(updateKycStatusSchema), approveOrRejectKyc);
-router.get('/', listKyc);
-router.get('/:id', getKycDetail);
+router.post("/", authMiddleware, validator.body(createKycSchema), createKyc);
+router.patch(
+  "/:id",
+  checkPermissionMiddleware(PERMISSIONS.KYC_REQUESTS),
+  validator.body(updateKycStatusSchema),
+  approveOrRejectKyc
+);
+router.get("/", checkPermissionMiddleware(PERMISSIONS.KYC_REQUESTS), listKyc);
+router.get(
+  "/:id",
+  checkPermissionMiddleware(PERMISSIONS.KYC_REQUESTS),
+  getKycDetail
+);
 
 export default router;
