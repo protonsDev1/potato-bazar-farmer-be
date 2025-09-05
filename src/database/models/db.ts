06ae46e902f8a8d1cea028e150d82003e6db9c57
protonsDev1 <shubham.joshi@protonshub.com>
2025-09-05 15:53:10 +0530

    intial prod commit

--- src/database/models/db.ts
+++ src/database/models/db.ts
@@ -9,7 +9,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD as string,
   {
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
   }
 );
 
