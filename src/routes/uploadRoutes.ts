7ac98ea13653840283569700dbf1ee91f5c8f293
Akash Mishra <akash.mishra@protonshub.in>
2025-09-23 19:05:25 +0530

    feat: Implement post API to upload single file to S3

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,8 +1,15 @@
 import express from "express";
-import { uploadToS3 } from "../utils/uploadFile";
+import multer from "multer";
+
+import { uploadDoc, uploadToS3 } from "../utils/uploadFile";
 
 const router = express.Router();
 
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
+
 router.get("/get-signed-url", uploadToS3);
+router.post("/upload-file", upload.single("file"), uploadDoc);
 
 export default router;
