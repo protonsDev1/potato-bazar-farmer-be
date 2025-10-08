94f48d53358e8b0d29e18398c318757573a1737b
Akash Mishra <akash.mishra@protonshub.in>
2025-10-08 18:43:49 +0530

    Modify cold storage profile completion api

--- src/utils/sesClient.ts
+++ src/utils/sesClient.ts
@@ -1,7 +1,7 @@
 import { SESClient } from "@aws-sdk/client-ses";
 
 export const sesClient = new SESClient({
-  region: process.env.S3_REGION,
+  region: process.env.SES_REGION,
   credentials: {
     accessKeyId: process.env.S3_ACCESS_KEY,
     secretAccessKey: process.env.S3_SECRET_KEY,
